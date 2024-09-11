3ea71d09269ba8b57c34b79e3e3de94de410402f
alexsuarez98 <111242870+alexsuarez98@users.noreply.github.com>
2024-09-11 17:50:04 +0000

    First card draw funciton

--- Main.py
+++ Main.py
@@ -1,1 +1,8 @@
-hello
+import random
+
+cards = ["Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9" "10", "Jack", "Queen", "King"]
+suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
+
+newCard = (random.choice(cards) + " of " + random.choice(suits))
+
+print("You have drawn the " + newCard)
