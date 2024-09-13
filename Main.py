a38ca1fe7d32f5e913c36bf455688b20f9117a42
alexsuarez98 <111242870+alexsuarez98@users.noreply.github.com>
2024-09-13 17:36:58 +0000

    Finished firstDrawCard function and implimented dealer hand checks.

--- Main.py
+++ Main.py
@@ -1,17 +1,38 @@
 import random
 
-cards = ["Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+cards = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
-deck = []
+playerDeck = []
+dealerHand = []
+
+def firstDrawCard ():
+    for i in range (0, 2):
+        newCard = (random.choice(cards) + " of " + random.choice(suits))
+        for i in playerDeck:
+           while newCard  in playerDeck or newCard in dealerHand:
+              newCard = (random.choice(cards) + " of " + random.choice(suits))
+        playerDeck.append(newCard)
+
+    for i in range (0, 2):
+        newCard = (random.choice(cards) + " of " + random.choice(suits))
+        for i in playerDeck:
+           while newCard  in playerDeck or newCard in dealerHand:
+              newCard = (random.choice(cards) + " of " + random.choice(suits))
+        dealerHand.append(newCard)
+    
+    
+    print("Player's Hand: " + ", " .join(playerDeck))
+    print("Player's Hand: " + ", " .join(dealerHand))
 
 def drawCard ():
     newCard = (random.choice(cards) + " of " + random.choice(suits))
-    print("You have drawn the " + newCard)
-    
-    deck.append(newCard)
-    print(deck)
+    for i in playerDeck:
+        while newCard  in playerDeck or newCard in dealerHand:
+            newCard = (random.choice(cards) + " of " + random.choice(suits))
+    playerDeck.append(newCard)
+    print("Player's Hand: " + ", " .join(playerDeck))
 
 print("START GAME")
-drawCard()
+firstDrawCard()
 
 
